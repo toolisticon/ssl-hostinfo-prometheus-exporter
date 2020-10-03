0d121c87552c9f81308031abcaee6ba481340381
Martin Reinhardt <contact@martinreinhardt-online.de>
2020-10-03 07:42:59 +0200

    chore(Build): Fix linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-const path = require('path'),
-    { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './app.js',
-    target: 'node',
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new CleanWebpackPlugin()
-    ],
+  entry: './app.js',
+  mode: 'production',
+  target: 'node',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 };

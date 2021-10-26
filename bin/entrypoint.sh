7a229f1825f223e563240b589a5ea3bc6d8eae73
Martin Reinhardt <contact@martinreinhardt-online.de>
2021-10-26 11:22:24 +0200

    fix(Build): Corrected packaging
    fixes #98

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,4 +4,4 @@ cd $HOME
 NVM_DIR="$HOME/.nvm"
 source $NVM_DIR/nvm.sh
 # TODO fix webpack build, see https://github.com/toolisticon/ssl-hostinfo-prometheus-exporter/issues/98
-node app.js
+node dist/app.js
--- test/end2end/run_end2end.sh
+++ test/end2end/run_end2end.sh
@@ -4,5 +4,9 @@ cd test/setup
 docker-compose rm -sf
 docker volume rm setup_prometheus_data
 docker-compose up -d --force-recreate
+
+echo "Waiting for results in mozilla stats"
+sleep 60
+
 cd ../..
 pytest test/end2end/test_*.py --junitxml=target/reports/junit.xml
